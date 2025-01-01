1c9740f0562bce80cce8f944b7d640b3b0399f1a
Abinesh C <cabinesh739@gmail.com>
2025-01-01 21:23:38 +0530

    Add files via upload

---
+++ student grade.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter your mark(0 to 100)\n"))
+if marks >= 90:
+    print("EXCELLENT")
+elif marks >= 80:
+    print("PRETTY GOOD")
+elif marks >= 70:
+    print("GOOD")
+elif marks >=60:
+    print("BETTER")
+elif marks >=50:
+    print("AVERAGE") 
+elif marks >=40:
+    print("poor") 
+else:
+    print("FAIL")                 
+   
